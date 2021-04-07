aa64fc34102e91b47007d8bc240dff7fd548c7ea
Chris Schade <chris.schade@gmail.com>
2021-04-07 11:07:26 +0200

    Excercise 4 - Perspective Decoration
    Added a component for the decoration (though it could just be a div in
    the CharacterEditor, I figured it's nice to be reuseable on other pages
    where we might want the same effect).
    
    It's simply a fixed positioned box. The maxwidthWrapper needs to become
    position: relative otherwise the perspectivedecoration would render
    above everything else (because of the fact that all static elements are
    rendered first and separately from fixedones)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import PerspectiveDecoration from './components/PerspectiveDecoration'
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <PerspectiveDecoration />
       <CharacterEditor />
       <Footer />
     </>
